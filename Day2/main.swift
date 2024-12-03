6e67816ed582657e2a611f5d29ea1291d8122f0b
Short, Tony (T.) <tshort15@ford.com>
2024-12-03 13:15:33 +0000

    Improved error reporting and moved code to top level of main

--- Day2/main.swift
+++ Day2/main.swift
@@ -7,25 +7,28 @@
 
 import Foundation
 
-print("Testing nuclear reactor data...")
-
 guard CommandLine.arguments.count > 1 else {
     print("Please supply file path")
     exit(1)
 }
-report(filePath: CommandLine.arguments[1])
 
-func report(filePath: String) {
-    guard let data = try? String(contentsOfFile: filePath, encoding: .utf8) else {
-        return
-    }
-
-    var successfulLevels = 0
-    data.enumerateLines(invoking: { line, _ in
-        let level = line.components(separatedBy: " ").map { Int($0) ?? 0 }
-        if PlantData.isLevelSafe(level) {
-            successfulLevels += 1
-        }
-    })
-    print("There were \(successfulLevels) successful levels")
+print("Analysing nuclear plant data...")
+guard let data = try? String(contentsOfFile: CommandLine.arguments[1], encoding: .utf8) else {
+    print("Failed to read input data")
+    exit(1)
 }
+
+var successfulLevels = 0
+data.enumerateLines(invoking: { line, _ in
+    let level = line.components(separatedBy: " ").map { report in
+        guard let reportNum = Int(report) else {
+            print("Invalid report \(report)")
+            exit(1)
+        }
+        return reportNum
+    }
+    if PlantData.isLevelSafe(level) {
+        successfulLevels += 1
+    }
+})
+print("There were \(successfulLevels) successful levels")
